34f4603515e3378b1ffe46be6c220b6743bab957
KCLouw <klouw07@gmail.com>
2023-02-02 17:20:51 +0200

    completed my even or odd challenge and it works as expected

--- evenOrOdd.js
+++ evenOrOdd.js
@@ -1,9 +1,10 @@
-let userInput = parseInt(document.getElementById("userInput").value);
+
 
 function myFunction(){
+    let userInput = parseInt(document.getElementById("userInput").value);
     if(isNaN(userInput)){
         document.getElementById("answer").innerHTML = "Please enter a numerical value";
-    }else if(userInput == 2 % 0){
+    }else if(userInput % 2 === 0){
         document.getElementById("answer").innerHTML = "Your number is even";
     }else{
         document.getElementById("answer").innerHTML = "Your number is odd";
